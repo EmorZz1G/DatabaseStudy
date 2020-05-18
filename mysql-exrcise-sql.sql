77cf73178118279c1ec86d18f6ded7ca146ed945
EmorZz1G <60733814+EmorZz1G@users.noreply.github.com>
2020-05-18 20:44:30 +0800

    6. 第二种理解，提高效率，上次提交没过脑子

--- mysql-exrcise-sql.sql
+++ mysql-exrcise-sql.sql
@@ -114,20 +114,15 @@ WHERE sc.`sno` IN
             (
                 SELECT student.sno
                 FROM student
-                WHERE sdept = "MA"
-            )
-# 6. 第二种理解
-SELECT student.`sname`,AVG(sc.`grade`)
-FROM sc,student
-WHERE sc.`sno` IN
-            (
-                SELECT student.sno
-                FROM student
                 WHERE sdept = "CS"
             )
+# 6. 第二种理解，提高效率，上次提交没过脑子
+SELECT student.`sname`,AVG(sc.`grade`)
+FROM sc,student
+WHERE 
+    student.`sdept`="CS"
     AND student.`sno`=sc.`sno`
 GROUP BY sc.`sno`
-
 # 7. 统计至少选修了两门课程的学生数
 SELECT COUNT(p.sno)
 FROM    
